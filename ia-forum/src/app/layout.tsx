0cbdf15ff11cf53ae0ad853eef926f365a0253c0
IAForumCoding <IAForumCoding@gmail.com>
2024-01-26 11:26:39 -0500

    added bootstrap

--- ia-forum/src/app/layout.tsx
+++ ia-forum/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import 'bootstrap/dist/css/bootstrap.css'
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -16,7 +17,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}</body>
     </html>
   );
 }
