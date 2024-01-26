d9e008886dc04559d324dc872f379eb2af8930d3
Yusuf Abdulahi <yusuffesih@gmail.com>
2024-01-26 14:36:01 -0500

    Added maybe working registration

---
+++ ia-forum/src/pages/api/register.ts
@@ -0,0 +1,36 @@
+import { PrismaClient } from '@prisma/client'
+import { NextApiRequest, NextApiResponse } from 'next'
+
+const prisma = new PrismaClient()
+
+async function addUser(studentID: string, firstName: string, lastName: string, email: string, password: string) {
+  await prisma.user.create({
+    data: {
+        student_id: parseInt(studentID),
+        student_first_name: firstName,
+        student_last_name: lastName,
+        email: email,
+        posts: {},
+        password: password,
+      },
+  })
+}
+
+export default function registrationHandler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === "POST") {
+    const {studentId, firstName, lastName, email, password} = req.body;
+
+    addUser(studentId, firstName, lastName, email, password)
+    .then(async () => {
+      await prisma.$disconnect()
+    })
+    .catch(async (e) => {
+      console.error(e)
+      await prisma.$disconnect()
+      process.exit(1)
+    })
+    res.status(201).redirect(201, "/login")
+  } else {
+    res.status(302).redirect(302, "/register")
+  }
+}
